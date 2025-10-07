585340e09fcc4cf65ecd30843abbb2d66a60d02d
Mateo <mateocarballo2@gmail.com>
2025-10-07 19:22:41 +0200

    Creado proyecto spring para API

---
+++ API/acces-control/src/main/java/com/controlacceso/accescontrol/ControlDeAccesoPymeApplication.java
@@ -0,0 +1,13 @@
+package com.controlacceso.accescontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlDeAccesoPymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlDeAccesoPymeApplication.class, args);
+	}
+
+}
---
+++ API/acces-control/src/test/java/com/controlacceso/accescontrol/ControlDeAccesoPymeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controlacceso.accescontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlDeAccesoPymeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
