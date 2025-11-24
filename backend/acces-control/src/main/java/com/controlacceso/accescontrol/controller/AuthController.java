fc23bce0757316c0c6b6e34e525221c27384f81f
Mateo <mateocarballo2@gmail.com>
2025-11-24 19:42:34 +0100

    Incluidos codigos de respuesta en funcion de la dto generada en el service

--- backend/acces-control/src/main/java/com/controlacceso/accescontrol/controller/AuthController.java
+++ backend/acces-control/src/main/java/com/controlacceso/accescontrol/controller/AuthController.java
@@ -20,15 +20,34 @@ public class AuthController {
     public AuthController(AuthService authService) {
         this.authService = authService;
     }
+
     @PostMapping("/login")
-    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO request){
-        return ResponseEntity.ok(authService.login(request));
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO request) {
+        LoginResponseDTO response = authService.login(request);
+        //Una vez construida la respuesta desde nuestro service, aquí definimos en código de la
+        //respuesta en función del mensaje generado. El caso 'default' es para otros errores no contemplados
+        if (response.token() == null) {
+
+            return switch (response.mensaje()) {
+                case "Contraseña incorrecta" -> ResponseEntity.status(401).body(response);
+
+                case "Usuario no encontrado" -> ResponseEntity.status(404).body(response);
+
+                case "Usuario inactivo" -> ResponseEntity.status(403).body(response);
+
+                default -> ResponseEntity.status(400).body(
+                        new LoginResponseDTO(null, null, "Error desconocido")
+                );
+            };
+        }
+
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/register")
-    public ResponseEntity<RegisterResponseDTO> register(@RequestBody RegisterRequestDTO request){
+    public ResponseEntity<RegisterResponseDTO> register(@RequestBody RegisterRequestDTO request) {
         RegisterResponseDTO response = authService.register(request);
-        if (!response.success()){
+        if (!response.success()) {
             return ResponseEntity.badRequest().body(response);
         }
         return ResponseEntity.ok(response);
